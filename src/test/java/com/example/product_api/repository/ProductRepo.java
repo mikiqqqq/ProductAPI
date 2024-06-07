7d52a42108eaa39e142b056a0789a00542eb436c
Filip Miloš <136697753+filipMiloshh@users.noreply.github.com>
2024-06-07 20:05:28 +0200

    updates

--- src/test/java/com/example/product_api/repository/ProductRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.product_api.repository;
-
-import com.example.product_api.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepo extends JpaRepository<Product, Integer> {
-}
